6f096b265ad44ce00d62ca61f3bafaed3cd91e00
andy.chen <405643529@qq.com>
2019-03-19 22:57:30 +0800

    功能：数据库连接工厂类

---
+++ Czar.Cms.Core/DbHelper/ConnectionFactory.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using Czar.Cms.Core.Extensions;
+using Czar.Cms.Core.Models;
+using MySql.Data.MySqlClient;
+using Npgsql;
+
+namespace Czar.Cms.Core.DbHelper
+{
+    /// <summary>
+    /// 数据库连接工厂类
+    /// </summary>
+    public class ConnectionFactory
+    {
+        /// <summary>
+        /// 获取数据库连接
+        /// </summary>
+        /// <param name="dbtype"></param>
+        /// <param name="strConn"></param>
+        /// <returns></returns>
+        public static IDbConnection CreateConnection(string dbtype, string strConn)
+        {
+            if (dbtype.IsNullOrWhiteSpace())
+            {
+                throw new ArgumentNullException("请上传数据库连接类型");
+            }
+
+            if (strConn.IsNullOrWhiteSpace())
+            {
+                throw new ArgumentNullException("请上传数据库连接字符串");
+            }
+
+            var dbType = GetDataBaseType(dbtype);
+            return CreateConnnection(dbType, strConn);
+        }
+
+
+        /// <summary>
+        /// 获取数据库连接
+        /// </summary>
+        /// <param name="dbType"></param>
+        /// <param name="strConn"></param>
+        /// <returns></returns>
+        public static IDbConnection CreateConnnection(DatabaseType dbType,string strConn)
+        {
+            IDbConnection connection = null;
+            if (strConn.IsNullOrWhiteSpace())
+            {
+                throw new ArgumentNullException("上传的数据库类型为空");
+            }
+
+            switch (dbType)
+            {
+                case DatabaseType.SqlServer:
+                    connection=new SqlConnection(strConn);
+                    break;
+                case DatabaseType.MySQL:
+                    connection=new MySqlConnection(strConn);
+                    break;
+                case DatabaseType.PostgreSQL:
+                    connection = new NpgsqlConnection(strConn);
+                    break;
+                    default:
+                        throw new ArgumentNullException($"暂时还不支持{dbType.ToString()}数据库类型");
+            }
+
+            if (connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+
+            return connection;
+
+        }
+
+        /// <summary>
+        /// 转换数据库类型
+        /// </summary>
+        /// <param name="dbtype">数据库类型字符串</param>
+        /// <returns>数据库类型</returns>
+        public static DatabaseType GetDataBaseType(string dbtype)
+        {
+            if (dbtype.IsNullOrWhiteSpace())
+            {
+                throw new ArgumentNullException("上传的数据库类型为空");
+            }
+
+            DatabaseType returnValue = DatabaseType.SqlServer;
+            foreach (DatabaseType dbType in Enum.GetValues(typeof(DatabaseType)))
+            {
+                if(dbType.ToString().Equals(dbtype,StringComparison.OrdinalIgnoreCase))
+                {
+                    returnValue = dbType;
+                    break;
+                }
+            }
+
+            return returnValue;
+        }
+        
+    }
+
+
+}
---
+++ Czar.Cms.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+
+namespace Czar.Cms.Core.Extensions
+{
+    /// <summary>
+    /// string扩展方法
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// 指示指定的字符串是null或者空字符串
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        [DebuggerStepThrough]
+        public static bool IsNullOrEmpty(this string value)
+        {
+            return string.IsNullOrEmpty(value);
+        }
+
+        /// <summary>
+        /// 指示指定的字符串是null,空或者仅由空白字符组成
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        [DebuggerStepThrough]
+        public static bool IsNullOrWhiteSpace(this string value)
+        {
+            return string.IsNullOrWhiteSpace(value);
+        }
+
+        /// <summary>
+        /// 指示指定的字符串是null,空或者仅由空白字符组成
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        [DebuggerStepThrough]
+        public static bool IsMissing(this string value)
+        {
+            return string.IsNullOrWhiteSpace(value);
+        }
+        
+    }
+}
